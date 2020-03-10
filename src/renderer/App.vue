dd236036f2a71295157398be9c5c80b342bfe2bc
unknown <motea927@yahoo.com.tw>
2020-03-10 15:18:36 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'mt-ticket'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
